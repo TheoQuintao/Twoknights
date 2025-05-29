4a4d18a94bfe259229fa28d19de2bd58d6d6a1bc
FritsOQ <0000900198@senaimgaluno.com.br>
2025-05-29 06:59:01 -0300

    Atualizar o Database.cs

--- Database.cs
+++ Database.cs
@@ -2,7 +2,7 @@ using MySqlConnector;
 
 public class Database
 {
-    private const string ConnectionString = "Server=localhost;Database=twoknights;User ID=root;Password=2507Theo@;";
+    private const string ConnectionString = "Server=localhost;Database=twoknights;User ID=root;Password=senha;";
 
     public static MySqlConnection GetConnection()
     {
